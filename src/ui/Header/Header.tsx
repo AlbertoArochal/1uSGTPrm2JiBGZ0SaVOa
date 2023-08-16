0b2fe12874a0014a148767cde126a7d2ae61ca5b
Alberto Rocha <albertorocha@pop-os.localdomain>
2023-08-16 14:29:13 +0100

    css fixes

--- src/ui/Header/Header.tsx
+++ src/ui/Header/Header.tsx
@@ -3,12 +3,18 @@ import styles from "./Header.module.css";
 import { Form } from "../../application/form/form";
 
 export function Header() {
-  return (
-    <header className={styles.header}>
-      <div className="siteName">
-        <img src={title} className="deadfulImg" alt="Deadful Cherry Tomatoes" />
-      </div>
-      <Form />
-    </header>
-  );
+    return (
+        <header className={styles.header}>
+            <div className="siteName">
+                <div className="deadfulImg__container">
+                    <img
+                        src={title}
+                        className="deadfulImg"
+                        alt="Deadful Cherry Tomatoes"
+                    />
+                </div>
+            </div>
+            <Form />
+        </header>
+    );
 }
