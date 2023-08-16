5fa8ac7a131ce02b72d35eb4d92c8682e1036cfa
Alberto Rocha <albertorocha@pop-os.localdomain>
2023-08-16 19:41:50 +0100

    added text to footer

--- src/ui/footer/footer.tsx
+++ src/ui/footer/footer.tsx
@@ -3,26 +3,39 @@ import appStore from "../../assets/button/app store.svg";
 import googlePlay from "../../assets/button/google play.svg";
 
 export const Footer = () => {
-  return (
-    <footer className="footer">
-      <section className="footer__box">
-        {" "}
-        <div className="footer__logo">
-          <img
-            src={title}
-            className="deadfulImg"
-            alt="Deadful Cherry Tomatoes"
-          />
-        </div>
-        <div className="footer__social">
-          <a href="https://www.apple.com/ios/app-store/">
-            <img src={appStore} className="appStore" alt="App Store" />
-          </a>
-          <a href="https://play.google.com/store">
-            <img src={googlePlay} className="googlePlay" alt="Google Play" />
-          </a>
-        </div>
-      </section>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <section className="footer__box">
+                {" "}
+                <div className="footer__logo">
+                    <img
+                        src={title}
+                        className="deadfulImg"
+                        alt="Deadful Cherry Tomatoes"
+                    />
+                </div>
+                <div className="footer__social">
+                    <a href="https://www.apple.com/ios/app-store/">
+                        <img
+                            src={appStore}
+                            className="appStore"
+                            alt="App Store"
+                        />
+                    </a>
+                    <a href="https://play.google.com/store">
+                        <img
+                            src={googlePlay}
+                            className="googlePlay"
+                            alt="Google Play"
+                        />
+                    </a>
+                </div>
+                <div className="footerText__container">
+                    <p className="footerText">
+                        © 2021 Deadful Cherry Tomatoes. All rights reserved.
+                    </p>
+                </div>
+            </section>
+        </footer>
+    );
 };
